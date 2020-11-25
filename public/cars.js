53e50b08e4e62fb9398ee24a423592718757f9ee
LuyoloLuja <luyolom19@gmail.com>
2020-11-25 13:00:08 +0200

    displayed other lists

--- public/cars.js
+++ public/cars.js
@@ -13,3 +13,31 @@ axios
             cars: result.data
         })
     })
+
+// colors
+const colorsList = document.querySelector('.colorList');
+const colorTemplate = document.querySelector('.colorTemp').innerHTML;
+const colorTemplateText = Handlebars.compile(colorTemplate);
+
+axios
+    .get('http://api-tutor.herokuapp.com/v1/colors')
+    .then(function (colorData) {
+        const html = colorTemplateText({
+            colors: colorData.data
+        })
+        colorsList.innerHTML = html;
+    })
+
+// brands
+const makesList = document.querySelector('.carMakes');
+const makesTemplate = document.querySelector('.makeTemp').innerHTML
+const makesTemplateText = Handlebars.compile(makesTemplate);
+
+axios
+    .get('http://api-tutor.herokuapp.com/v1/makes')
+    .then(function (makesData) {
+        // console.log(makesData);
+        makesList.innerHTML = makesTemplateText({
+            brands: makesData.data
+        })
+    })
